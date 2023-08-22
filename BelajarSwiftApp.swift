a7633c9bb9d48352fd7c1916599305fa9a94f821
Yoga Febriatala <yogafebriatala24@gmail.com>
2023-08-23 00:59:02 +0700

    first commit

---
+++ BelajarSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelajarSwiftApp.swift
+//  BelajarSwift
+//
+//  Created by Yoga Febriatala on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BelajarSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
